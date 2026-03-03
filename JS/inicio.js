e1dba9c3dd3222146c84111433a638d8826ee776
Rey Picazo <emamhdjej@gmail.com>
2026-03-03 11:07:16 -0600

    Añadi la redireccion de todos los botones

--- JS/inicio.js
+++ JS/inicio.js
@@ -17,3 +17,9 @@ document.getElementById("btnInicio").href = "../Publi/inicio.html";
 document.getElementById("btnMisSorteos").href = "../Publi/misSorteos.html";
 
 document.getElementById("nameUsuario").textContent = localStorage.getItem("usuario");
+
+document.getElementById("btnCerrarSesion").addEventListener("click", function(e) {
+    e.preventDefault();
+    localStorage.clear();
+    window.location.href = "../Publi/presentacion.html";
+});
